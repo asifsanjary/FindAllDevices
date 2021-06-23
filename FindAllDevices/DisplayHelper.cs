2e117f678e2684615c5d46bbda5f03733791d721
Asif Sanjary <0asifsanjary@gmail.com>
2021-06-23 21:46:59 +0600

    Add project files.

---
+++ FindAllDevices/DisplayHelper.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Windows.Devices.Enumeration;
+using Windows.UI.Xaml.Media.Imaging;
+
+// NOTE: The following using statements are only needed in order to demonstrate many of the
+// different device selectors available from Windows Runtime APIs. You will only need to include
+// the namespace for the Windows Runtime API your actual scenario needs.
+
+namespace FindAllDevices
+{
+
+    public class DeviceInformationDisplay : INotifyPropertyChanged
+    {
+        public DeviceInformationDisplay(DeviceInformation deviceInfoIn)
+        {
+            DeviceInformation = deviceInfoIn;
+            UpdateGlyphBitmapImage();
+        }
+
+        public DeviceInformationKind Kind => DeviceInformation.Kind;
+        public string Id => DeviceInformation.Id;
+        public string Name => DeviceInformation.Name;
+        public BitmapImage GlyphBitmapImage { get; private set; }
+        public bool CanPair => DeviceInformation.Pairing.CanPair;
+        public bool IsPaired => DeviceInformation.Pairing.IsPaired;
+        public IReadOnlyDictionary<string, object> Properties => DeviceInformation.Properties;
+        public DeviceInformation DeviceInformation { get; private set; }
+
+        public void Update(DeviceInformationUpdate deviceInfoUpdate)
+        {
+            DeviceInformation.Update(deviceInfoUpdate);
+
+            OnPropertyChanged("Kind");
+            OnPropertyChanged("Id");
+            OnPropertyChanged("Name");
+            OnPropertyChanged("DeviceInformation");
+            OnPropertyChanged("CanPair");
+            OnPropertyChanged("IsPaired");
+            OnPropertyChanged("GetPropertyForDisplay");
+
+            UpdateGlyphBitmapImage();
+        }
+
+        public string GetPropertyForDisplay(string key) => Properties[key]?.ToString();
+
+        private async void UpdateGlyphBitmapImage()
+        {
+            DeviceThumbnail deviceThumbnail = await DeviceInformation.GetGlyphThumbnailAsync();
+            BitmapImage glyphBitmapImage = new BitmapImage();
+            await glyphBitmapImage.SetSourceAsync(deviceThumbnail);
+            GlyphBitmapImage = glyphBitmapImage;
+            OnPropertyChanged("GlyphBitmapImage");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
